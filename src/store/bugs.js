ffc669e2b1138350ece6229cae33366702a27bce
Sunny <sanyasirao@ibhubs.co>
2021-06-04 11:10:20 +0530

    Refactor reducer, actions with create slice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,28 +1,28 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-//actions
-export const bugAdded = createAction('bugAdded')
-export const bugRemoved = createAction('bugRemoved')
-export const bugResolved = createAction('bugResolved')
-
-//Reducers
 let lastId = 0
 
-const reducer = createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      resolved: false,
-      description: action.payload.description,
-    })
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const bugIndex = bugs.findIndex((bug) => bug.id === action.payload.id)
-    bugs[bugIndex].resolved = true
-  },
-  [bugRemoved.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id !== action.pay)
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        resolved: false,
+        description: action.payload.description,
+      })
+    },
+    bugResolved: (bugs, action) => {
+      const bugIndex = bugs.findIndex((bug) => bug.id === action.payload.id)
+      bugs[bugIndex].resolved = true
+    },
+    bugRemoved: (bugs, action) => {
+      bugs.filter((bug) => bug.id !== action.pay)
+    },
   },
 })
 
-export default reducer
+export const { bugAdded, bugResolved, bugRemoved } = slice.actions
+
+export default slice.reducer
