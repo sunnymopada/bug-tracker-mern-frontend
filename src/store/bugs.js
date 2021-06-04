38d3b2862fd537e4f1345982fb06f294ac56d351
Sunny <sanyasirao@ibhubs.co>
2021-06-04 11:01:47 +0530

    Refactor reducer using redux tool kit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 //actions
 export const bugAdded = createAction('bugAdded')
@@ -8,30 +8,21 @@ export const bugResolved = createAction('bugResolved')
 //Reducers
 let lastId = 0
 
-function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          resolved: false,
-          description: action.payload.description,
-        },
-      ]
-    case bugRemoved.type:
-      return state.filter((bug) => bug.id !== action.payload.id)
-    case bugResolved.type:
-      const bugIndex = state.findIndex((bug) => bug.id === action.payload.id)
-      const bug = state[bugIndex]
-      return [
-        ...state.slice(0, bugIndex),
-        { ...bug, resolved: true },
-        ...state.slice(bugIndex + 1),
-      ]
-    default:
-      return state
-  }
-}
+const reducer = createReducer([], {
+  [bugAdded.type]: (bugs, action) => {
+    bugs.push({
+      id: ++lastId,
+      resolved: false,
+      description: action.payload.description,
+    })
+  },
+  [bugResolved.type]: (bugs, action) => {
+    const bugIndex = bugs.findIndex((bug) => bug.id === action.payload.id)
+    bugs[bugIndex].resolved = true
+  },
+  [bugRemoved.type]: (bugs, action) => {
+    bugs.filter((bug) => bug.id !== action.pay)
+  },
+})
 
 export default reducer
