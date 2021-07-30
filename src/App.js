98a8d2965afd3250771dc190226e89c946b672b5
Sunny <sanyasirao@ibhubs.co>
2021-07-30 17:58:29 +0530

    Fix gh issues with BrowserRouter itself

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import { Provider } from 'react-redux'
-import { Switch, HashRouter as Router } from 'react-router-dom'
+import { Switch, BrowserRouter as Router } from 'react-router-dom'
 
 import store from './store/configureStore'
 
--- src/constants/NavigationConstants.js
+++ src/constants/NavigationConstants.js
@@ -1,4 +1,4 @@
-export const REGISTER_ROUTE_PATH = '/register'
-export const LOGIN_ROUTE_PATH = '/login'
-export const DASHBOARD_ROUTE_PATH = '/dashboard'
-export const HOME_ROUTE_PATH = '/'
+export const REGISTER_ROUTE_PATH = `${process.env.PUBLIC_URL}/register`
+export const LOGIN_ROUTE_PATH = `${process.env.PUBLIC_URL}/login`
+export const DASHBOARD_ROUTE_PATH = `${process.env.PUBLIC_URL}/dashboard`
+export const HOME_ROUTE_PATH = `${process.env.PUBLIC_URL}/`
